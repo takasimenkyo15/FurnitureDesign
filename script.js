9dce9e5a6e88da81a3e9aa56e5a4cadd02b1ca2f
takasimenkyo15 <99574137+takasimenkyo15@users.noreply.github.com>
2022-02-26 12:16:11 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function() {
+
+  
+
+  $(".openbtn").click(function () {//ボタンがクリックされたら
+    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+  });
+  
+  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+      $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+      $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+  });
+ 
+$('#views').click(function() {
+  $('#view').css('display','block');
+  $('#views').css('display','none')
+});
+
+
+
+  
+  
+  });
